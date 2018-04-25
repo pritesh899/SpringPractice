c4892434bbd7ebe6945813d8b881f8dfec4c62b8
Pritesh Ratnappagol <pritesh.ratnappagol@gmail.com>
2018-04-25 00:52:53 -0400

    Add files via upload

---
+++ ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package com.pritesh.us.net.example.NetSpring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.programmer.gate")
+public class ApplicationConfiguration {
+}
